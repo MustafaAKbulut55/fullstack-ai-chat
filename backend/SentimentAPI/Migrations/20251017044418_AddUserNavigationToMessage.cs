0468a18995ea824b11f438f4523c0b862f33ca92
MustafaAKbulut55 <akbulutmustafa081@gmail.com>
2025-10-17 07:45:05 +0300

    a
    a

---
+++ backend/SentimentAPI/Migrations/20251017044418_AddUserNavigationToMessage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SentimentAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserNavigationToMessage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
